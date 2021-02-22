e2801df6fb0ebcd011572e5de51c70fd00171054
luisga32 <luisga32@hotmail.com>
2021-02-22 20:29:37 +0100

    done change

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const User = require("../models/user.model")
+const User = require("../models/user.model");
 
 module.exports.register = (req, res, next) => {
 
