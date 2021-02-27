e14c7122b58b96648826b17539f356c01b9dc3fc
joselrdg <josesietepicos@gmail.com>
2021-02-27 17:44:53 +0100

    reproductor

--- public/javascripts/youtube.js
+++ public/javascripts/youtube.js
@@ -1,30 +1,44 @@
 
+window.addEventListener('load', () => {
 
+ const dataPlayList =  document.querySelectorAll(".gPlaylist")
+ dataPlayList.forEach(card=> {
+    //  console.log(card)
 
-// let apiUrl = 'https://developers.google.com/apis-explorer/#p/youtube/v3/youtube.playlists.list?part=snippet,contentDetails&channelId=UC_x5XG1OV2P6uZZ5FSM9Ttw'
+   
+     card.addEventListener("click", function (event) {
+       
+        console.log(event.target.dataset.id)
+        videoPlayer(event.target.dataset.id)
 
+    })
+ })
+})
 
-
-//  document.getElementById("__ytb").
-//     addEventListener("click", function () {
-      
-//         console.log('ESTAMOS  AddEvent')
-
-//     });
-
-
- function funAxios() {
-    axios
-        .get(apiUrl, config)
-        .then((response) => {
-            const { data } = response;
-            console.log(data)
-        })
-        .catch((e) => console.error("Error getting data", e));
-
-    const paintData = () => {
-        
-    }
+const videoPlayer = (id) => {
+    document.querySelector('.videoPlayer').innerHTML = "";
+    let div = document.createElement("div");
+    div.className = "vPlayer";
+    div.innerHTML = `
+      <iframe id="ytplayer" type="text/html" width="640" height="360" src="http://www.youtube.com/embed?listType=playlist&list=${id}"
+  frameborder="0" autoplay="1"/>`
+    document.querySelector('.videoPlayer').appendChild(div);
 }
 
-// module.exports = fun;
+ 
+
+//  function funAxios() {
+//         axios
+//             .get(apiUrl, config)
+//             .then((response) => {
+//                 const { data } = response;
+//                 console.log(data)
+//             })
+//             .catch((e) => console.error("Error getting data", e));
+
+//         const paintData = () => {
+
+//         }
+//     }
+
+// module.exports = fun
